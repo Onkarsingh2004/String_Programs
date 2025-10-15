d1ddc55dd23c9bcd9966a41e17d96333de778588
Onkarsingh2004 <Onkar620singh@gmail.com>
2025-10-15 23:02:40 +0530

    .

---
+++ FrequencyString.java
@@ -0,0 +1,14 @@
+public class FrequencyString {
+    public static void main(String[] args) {
+        String str="HelloOnkar";
+        int[]freq=new int[256];
+        for(int i=0; i<str.length(); i++){
+            freq[str.charAt(i)]++;
+        }
+        for(int i=0; i<256; i++){
+            if(freq[i]!=0){
+                System.out.println((char)i +" = "+freq[i]);
+            }
+        }
+    }
+}
